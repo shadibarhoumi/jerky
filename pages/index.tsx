72a259b6855b633dd81447fb33b0ade809cc9c2f
Shadi Barhoumi <shadibarhoumi@gmail.com>
2021-02-03 09:08:11 -0700

    Initial setup with Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+
+export const HomePage: React.FC = () => {
+  return (
+    <>
+      <Head>Shadi's Blog</Head>
+      <p>This is Shadi's blog</p>
+    </>
+  )
+}
+
+export default HomePage
